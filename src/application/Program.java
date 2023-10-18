cee77c967a701e1c28d38a576c13efb5cfdbc14b
Gui-Sardinha <71841173+Guilherme-Sardinha@users.noreply.github.com>
2023-10-18 14:42:44 -0300

    Mudanca na classe Board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Integer i = 0;
+		
+		Position position = new Position(3, 2);
+		
+		System.out.println(position);
+		
 	}
 
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -14,14 +14,27 @@ public class Position {
 	public Integer getRow() {
 		return row;
 	}
+	
+	public void setRow(Integer row) {
+		this.row = row;
+	}
 
 	public Integer getColumn() {
 		return column;
 	}
 	
+	public void Column(Integer column) {
+		this.column = column;
+	}
+	
 	public void setValues(Integer row, Integer column) {
 		this.column = column;
 		this.row = row;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
 	}
 
 }
